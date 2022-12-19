02e27b37050496e33b756daf3294e9044a53bcc9
Adrián <jagv94@gmail.com>
2022-12-19 11:18:32 +0100

    Adding jump sound when jumps

--- js/playerPrefab.js
+++ js/playerPrefab.js
@@ -357,6 +357,7 @@ class playerPrefab extends actorPrefab {
                             this.body.blocked.down && !this.canClimbLadder &&
                             Phaser.Input.Keyboard.DownDuration(this.cursorKeys.up, 250)) {
                             this.body.setVelocityY(-gamePrefs.ARTHUR_JUMP);
+                            game.sound.play('arthurJump');
                         }
                         else if (this.canClimbLadder && this.cursorKeys.up.isDown &&
                             this.body.blocked.down && this.cursorKeys.up.isDown) {
