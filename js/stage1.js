313a8dd77eecbab52daee4c86ff137194be0db7c
Fabio Morini <fabio.morini@enti.cat>
2023-01-05 13:16:11 +0100

    Added Score Text and top score text + points

--- js/stage1.js
+++ js/stage1.js
@@ -1,6 +1,8 @@
 class stage1 extends Phaser.Scene {
   constructor() {
     super({ key: "stage1" });
+
+    this.playerPointsText;
   }
 
   preload() {}
@@ -71,7 +73,16 @@ class stage1 extends Phaser.Scene {
     //ESTO SE PUEDE ELIMINAR SI YA NO SE VA A ABRIR LA PUERTA CON "CONTROL"
     this.openDoorKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
  
+    this.gameHUD();
     this.timer();
+
+    this.playerPointsText = this.add.bitmapText(
+      10,
+      10,
+      "arcadeFont",
+      ""
+    ).setScale(0.28).setScrollFactor(0);
+
   }
 
   update() {
@@ -89,6 +100,9 @@ class stage1 extends Phaser.Scene {
     if(this.openDoorKey.isDown){
       this.door.openDoor();
     }
+
+    //actualizar hud score player
+    this.playerPointsText.text = gamePrefs.score;
   }
 
   inputScene() {
@@ -96,18 +110,48 @@ class stage1 extends Phaser.Scene {
     this.scene.start("InputScene");
   }
 
+  gameHUD()
+  {
+    var playerText = this.add.bitmapText(
+      5,
+      1,
+      "arcadeFont",
+      "PLAYER 1"
+    ).setScale(0.28).setScrollFactor(0);
+
+    var topScoreText = this.add.bitmapText(
+      100,
+      1,
+      "arcadeFont",
+      "TOP SCORE"
+    ).setScale(0.28).setScrollFactor(0).setTint(0xcb3058);
+
+    var topPointsText = this.add.bitmapText(
+      135,
+      10,
+      "arcadeFont",
+      "10000"
+    ).setScale(0.28).setScrollFactor(0);
+  }
+
   timer()
   {
     var timerValue = 2 * 60;
 
-    var timerText = this.add.bitmapText(
-      1,
-      1,
+    var timeText = this.add.bitmapText(
+      10,
+      30,
       "arcadeFont",
-      "02:00"
-    ).setScale(0.36);
+      "TIME"
+    ).setScale(0.28).setScrollFactor(0);
 
-    timerText.setScrollFactor(0);
+    var timerText = this.add.bitmapText(
+      10,
+      40,
+      "arcadeFont",
+      "2 00"
+    ).setScale(0.28).setScrollFactor(0);
+
 
     var timerAux = this.time.addEvent({
       delay: 1000,
@@ -117,14 +161,14 @@ class stage1 extends Phaser.Scene {
         var seconds = Phaser.Math.FloorTo(timerValue % 60);
 
         // Add leading zeros to the minutes and seconds, if necessary
-        minutes = Phaser.Utils.String.Pad(minutes, 2, "0", 1);
+        minutes = Phaser.Utils.String.Pad(minutes, 2, "", 1);
         seconds = Phaser.Utils.String.Pad(seconds, 2, "0", 1);
 
         // Decrement the timer value
         timerValue --;
 
         // Update the Text object with the new time
-        timerText.text = minutes + ":" + seconds;
+        timerText.text = minutes + " " + seconds;
 
       },
       callbackScope: this,
